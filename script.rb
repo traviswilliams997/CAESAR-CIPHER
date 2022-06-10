6e46e08ffd8f4d2da26a026ba8c9ed92b665fb37
Travis Williams <traviswilliams997@gmail.com>
2022-06-10 00:47:30 -0500

    fix: Allow caesar_cipher to work with uppercase

--- script.rb
+++ script.rb
@@ -1,11 +1,11 @@
 def caesar_cipher(strings, num)
     for pos in 0 ...strings.length do 
-        if strings[pos] >='a' && strings[pos] <= 'z'  
+        if strings[pos] >='a' && strings[pos] <= 'z' || strings[pos] >='A' && strings[pos] <= 'Z'
              p (strings[pos].ord() + num ).chr
 
         end
     end
 end
 
-caesar_cipher("abc", 5);
+caesar_cipher("aBc", 5);
 
