8d49fe59bc5d0a71c54ada7ef5e4c92e6b8f1064
Travis Williams <traviswilliams997@gmail.com>
2022-06-10 11:54:30 -0500

    refactor: Change name for   character in string varaible

--- script.rb
+++ script.rb
@@ -1,17 +1,17 @@
 def caesar_cipher(string, shift_value)    
   new_string = ''
   for pos in 0...string.length do 
-    char = string[pos]
+    current_char = string[pos]
     #converts to ascii value 
     ascii_num = string[pos].ord() 
-    if char >='a' && char <= 'z' 
+    if current_char >='a' && current_char <= 'z' 
       # z to a wrap logic
       if (ascii_num + shift_value) <= 122 
         new_string += (ascii_num  + shift_value ).chr
       else
         new_string += ((ascii_num + shift_value ) - 123 + 97).chr
       end
-    elsif char >='A' && char <= 'Z' 
+    elsif current_char >='A' && current_char <= 'Z' 
       # Z to A wrap logic
       if(ascii_num + shift_value) <= 90 
         new_string += (ascii_num + shift_value ).chr
@@ -20,7 +20,7 @@ def caesar_cipher(string, shift_value)
       end
     else 
       # non letter characters
-      new_string += char
+      new_string += current_char
         
         end
     end
