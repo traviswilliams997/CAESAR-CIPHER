f3bdea78cb996ced97cc3d4b6c2bf5782c9f2b29
Travis Williams <traviswilliams997@gmail.com>
2022-06-10 01:13:56 -0500

    fix: z to a wrap

--- script.rb
+++ script.rb
@@ -1,12 +1,22 @@
-def caesar_cipher(strings, num)
+def caesar_cipher(string, num)
     new_string = ''
-    for pos in 0 ...strings.length do 
-        if strings[pos] >='a' && strings[pos] <= 'z' || strings[pos] >='A' && strings[pos] <= 'Z'
-            new_string += (strings[pos].ord() + num ).chr
+    for pos in 0 ...string.length do 
+        if string[pos] >='a' && string[pos] <= 'z' 
+             if (string[pos].ord() + num) <= 122 
+            new_string += (string[pos].ord() + num ).chr
+             else
+                new_string += ((string[pos].ord() + num ) - 123 + 97).chr
+             end
 
-        else
-            new_string += strings[pos]
-        end
+        elsif string[pos] >='A' && string[pos] <= 'Z' 
+            if(string[pos].ord() + num) <= 90 
+                new_string += (string[pos].ord() + num ).chr
+            else
+                new_string += ((string[pos].ord() + num ) - 91 + 65).chr    
+            end
+        else 
+                new_string += string[pos]
+            end
     end
    p new_string
 end
