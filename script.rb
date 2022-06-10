0537af47c079ffa6b0380fca361a0c797ef7c0d4
Travis Williams <traviswilliams997@gmail.com>
2022-06-10 00:54:49 -0500

    fix: Print shifted string instead of list of shifted chars

--- script.rb
+++ script.rb
@@ -1,11 +1,13 @@
 def caesar_cipher(strings, num)
+    new_string = ''
     for pos in 0 ...strings.length do 
         if strings[pos] >='a' && strings[pos] <= 'z' || strings[pos] >='A' && strings[pos] <= 'Z'
-             p (strings[pos].ord() + num ).chr
+            new_string += (strings[pos].ord() + num ).chr
 
         end
     end
+   p new_string
 end
 
-caesar_cipher("aBc", 5);
+caesar_cipher("What a string!", 5);
 
