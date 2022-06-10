fe694786e3bb72bb8c4a1269ba8fab55ae03df94
Travis Williams <traviswilliams997@gmail.com>
2022-06-10 12:03:20 -0500

    refactor: Create variable for conversion of shifted ascii number to character

--- script.rb
+++ script.rb
@@ -2,21 +2,21 @@ def caesar_cipher(string, shift_value)
   new_string = ''
   for pos in 0...string.length do 
     current_char = string[pos]
-    #converts to ascii value 
     ascii_num = current_char.ord() 
     new_ascii_num = ascii_num + shift_value
+    new_char = new_ascii_num.chr
 
     if current_char >='a' && current_char <= 'z' 
       # z to a wrap logic
       if new_ascii_num  <= 122 
-        new_string += new_ascii_num.chr
+        new_string += new_char
       else
         new_string += ( new_ascii_num - 123 + 97).chr
       end
     elsif current_char >='A' && current_char <= 'Z' 
       # Z to A wrap logic
       if new_ascii_num  <= 90 
-        new_string += new_ascii_num.chr
+        new_string += new_char
       else
         new_string += (new_ascii_num - 91 + 65).chr    
       end
@@ -24,8 +24,8 @@ def caesar_cipher(string, shift_value)
       # non letter characters
       new_string += current_char
         
-        end
     end
+  end
    p new_string
 end
 
