1047cf99b11de0fb67e34c92b82486e36862f82d
Travis Williams <traviswilliams997@gmail.com>
2022-06-11 12:54:52 -0500

    refactor: Change name for shifted character

--- script.rb
+++ script.rb
@@ -10,19 +10,19 @@ def caesar_cipher(string, shift_value)
     current_char = string[pos]
     ascii_num =  convert_character_to_ascii_num(current_char)
     shifted_ascii_num = shift_ascii_num(ascii_num, shift_value)
-    new_char = shifted_ascii_num.chr
+    shifted_char = shifted_ascii_num.chr
 
     if current_char >='a' && current_char <= 'z' 
       # z to a wrap logic
       if shifted_ascii_num  <= 122 
-        new_string += new_char
+        new_string += shifted_char
       else
         new_string += ( shifted_ascii_num - 123 + 97).chr
       end
     elsif current_char >='A' && current_char <= 'Z' 
       # Z to A wrap logic
       if shifted_ascii_num  <= 90 
-        new_string += new_char
+        new_string += shifted_char
       else
         new_string += (shifted_ascii_num - 91 + 65).chr    
       end
