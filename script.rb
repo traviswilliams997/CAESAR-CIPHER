f7fdcab27577e85a62946b2f0226f33cd2dac1e7
Travis Williams <traviswilliams997@gmail.com>
2022-06-11 12:59:55 -0500

    refactor: Create function to check if current charcter is lower case

--- script.rb
+++ script.rb
@@ -7,6 +7,9 @@ end
 def convert_ascii_num_to_character(ascii)
   ascii.chr
 end
+def check_if_character_is_lowercase_letter(character)
+  character >='a' && character <= 'z'
+end
 def caesar_cipher(string, shift_value)    
   new_string = ''
   for pos in 0...string.length do 
@@ -15,7 +18,7 @@ def caesar_cipher(string, shift_value)
     shifted_ascii_num = shift_ascii_num(ascii_num, shift_value)
     shifted_char = convert_ascii_num_to_character(shifted_ascii_num)
 
-    if current_char >='a' && current_char <= 'z' 
+    if check_if_character_is_lowercase_letter(current_char)
       # z to a wrap logic
       if shifted_ascii_num  <= 122 
         new_string += shifted_char
