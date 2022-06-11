43d3d97e30460eef601e06f978302fb6066f466c
Travis Williams <traviswilliams997@gmail.com>
2022-06-11 14:04:07 -0500

    refactor: Change variable names

--- script.rb
+++ script.rb
@@ -1,13 +1,13 @@
-def convert_character_to_ascii_num(character)
-  character.ord() 
+def convert_character_to_ascii_num(current_character)
+  current_character.ord() 
 end
 
 def shift_ascii_num(num, value)
   num + value
 end
 
-def convert_ascii_num_to_character(ascii)
-  ascii.chr
+def convert_ascii_num_to_character(num)
+  num.chr
 end
 
 def check_if_character_is_lowercase_letter(character)
@@ -36,30 +36,30 @@ end
 
 
 def caesar_cipher(string, shift_value)    
-  new_string = ''
-  string.each_char do |char| 
-    ascii_num =  convert_character_to_ascii_num(char)
-    shifted_ascii_num = shift_ascii_num(ascii_num, shift_value)
-    shifted_char = convert_ascii_num_to_character(shifted_ascii_num)
+  shifted_string = ''
+  string.each_char do |current_character| 
+    ascii_number =  convert_character_to_ascii_num(current_character)
+    shifted_ascii_number = shift_ascii_num(ascii_number, shift_value)
+    shifted_character = convert_ascii_num_to_character(shifted_ascii_number)
 
-    if check_if_character_is_lowercase_letter(char)
-      if check_if_lowercase_ascii_num_is_in_letter_range(shifted_ascii_num)
-        new_string += shifted_char
+    if check_if_character_is_lowercase_letter(current_character)
+      if check_if_lowercase_ascii_num_is_in_letter_range(shifted_ascii_number)
+        shifted_string += shifted_character
       else
-        new_string += convert_ascii_num_to_character(wrap_to_start_lowercase(shift_ascii_num))
+        shifted_string += convert_ascii_num_to_character(wrap_to_start_lowercase(shift_ascii_number))
       end
-    elsif check_if_character_is_uppercase_letter(char) 
-      if check_if_uppercase_ascii_num_is_in_letter_range(shifted_ascii_num)
-        new_string += shifted_char
+    elsif check_if_character_is_uppercase_letter(current_character) 
+      if check_if_uppercase_ascii_num_is_in_letter_range(shifted_ascii_number)
+        shifted_string += shifted_character
       else
-        new_string += convert_ascii_num_to_character(wrap_to_start_uppercase(shifted_ascii_num))    
+        shifted_string += convert_ascii_num_to_character(wrap_to_start_uppercase(shifted_ascii_number))    
       end
     else 
-      new_string += char
+      shifted_string += current_character
         
     end
   end
-   p new_string
+   p shifted_string
 end
 
 
