3a2178c573daea5695cc2aff60a3ad3e8608d66b
Travis Williams <traviswilliams997@gmail.com>
2023-01-01 14:54:22 -0500

    Add tests

--- spec/cipher_spec.rb
+++ spec/cipher_spec.rb
@@ -2,7 +2,20 @@ require 'rspec'
 require '../script'
 
 describe '#caesar_cipher' do
+    it 'returns "A"' do
+        expect(caesar_cipher("A", 0)).to eql('A')
+    end
+    it 'returns "F"' do 
+        expect(caesar_cipher("A", 5)).to eql('F')
+    end
+    it 'returns "A"' do
+        expect(caesar_cipher("A", 26)).to eql('A')
+    end
     it 'returns "Bmfy f xywnsl!"' do
         expect(caesar_cipher("What a string!", 5)).to eql('Bmfy f xywnsl!')
     end
+    it 'returns "What a string!""' do
+        expect(caesar_cipher("What a string!", 0)).to eql('What a string!')
+    end
+    
 end
